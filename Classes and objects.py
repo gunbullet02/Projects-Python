571391186a0710241cf97deed02359cb09b25c70
Josh Nathan Josol <61985305+gunbullet02@users.noreply.github.com>
2020-05-16 14:18:38 +0800

    Add files via upload

---
+++ Classes and objects.py
@@ -0,0 +1,15 @@
+class myclass:
+    variable = "Josh"
+
+    def self(self):
+        print("This a message inside the class")
+
+myobjectx = myclass()
+
+myobjectx.function()
+#myobjecty = myclass()
+
+#myobjecty.variable = "Josol"
+
+#print(myobjectx.variable)
+#print(myobjecty.variable)
